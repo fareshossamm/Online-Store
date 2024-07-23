0132a5dacbfef0376faf150431f9514b69a7853e
Fares Hossam <fareshossam201@gmail.com>
2024-07-23 08:08:21 +0300

    fix error in routing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 import Navbar from './components/Navbar';
 import Slider from './components/Slider';
 import ProductsList from './components/ProductsList';
+import { Routes, Route } from "react-router-dom";
 import About from './components/About';
 import ProductDetails from './components/ProductDetails';
-import ContactUs from './components/ContactUs'; // Fixed import name
+import ContactUs from './components/ContactUs';
 import Services from './components/Services';
 import Cart from './components/Cart';
 import Sales from './components/Sales';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Slider />
-              <ProductsList />
-              <Sales />
-            </>
-          } />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} /> {/* Fixed component name */}
-          <Route path="/services" element={<Services />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product/:productId" element={<ProductDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Slider />
+            <ProductsList />
+            <Sales />
+            
+          </>
+        } />
+        <Route path="/online-store" element={
+          <>
+            <Slider />
+            <ProductsList />
+            <Sales />
+          </>
+        } />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+      </Routes>
+      
+    </div>
   );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Proper BrowserRouter import
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Correct Bootstrap CSS import
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Correct Bootstrap JS import
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Correct Bootstrap JS import (if needed)
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
